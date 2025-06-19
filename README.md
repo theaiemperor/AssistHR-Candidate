93351fc9b13d037cadbe4ab745b20bd1067bc3c9
Arman <119550818+theaiemperor@users.noreply.github.com>
2025-06-19 09:10:17 +0530

    Setting up project

---
+++ README.md
@@ -0,0 +1,2 @@
+## About
+An ai interviewer
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div className={'flex flex-col justify-center '}>
+            <img className={'w-50 self-center mt-5'} src={'/logo.png'} alt={'logo'} />
+            <div className={'text-xl text-center mt-5'}>
+                hello, from interviewer
+            </div>
+        </div>
+
+    );
+}
